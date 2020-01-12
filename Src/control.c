4abb8a3f6cd863f5d73aa1d25f9feb710cb58e34
song23333 <1609015886@qq.com>
2020-01-12 08:38:44 +0800

    重配传球10号电机  但通讯有点问题

--- Src/control.c
+++ Src/control.c
@@ -123,7 +123,7 @@ void DriverInit(void)
 		#elif CAN_ID_NUM == 10	//����
 			Driver.ControlMode = SPD_CURR_CTRL_MODE;
 			DriverCtrlModeInit();
-			PosSensor.PosOffset = 6599;
+			PosSensor.PosOffset = 15661;
 			CurrLoop.LimitCurrQ = 100.f;
 			SpdLoop.ExptMecAngularSpeed_rad = -70.f * 2 * PI;
 			SpdLoop.Kp = SPEED_CONTROL_KP * 1.0f;	

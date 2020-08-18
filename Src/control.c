dd4ef1a10ef3929f062b4d373d433d015726652b
song23333 <1609015886@qq.com>
2020-08-18 17:10:47 +0800

    no message

--- Src/control.c
+++ Src/control.c
@@ -191,7 +191,7 @@ void DriverInit(void)
 		#elif CAN_ID_NUM == 7
 			Driver.ControlMode = SPD_CURR_CTRL_MODE;
 			DriverCtrlModeInit();
-			PosSensor.PosOffset = 13044;
+			PosSensor.PosOffset = 31724;
 			CurrLoop.LimitCurrQ = 20.f;
 			SpdLoop.ExptMecAngularSpeed_rad = 0.f * 2 * PI;
 			SpdLoop.Kp = SPEED_CONTROL_KP * 1.0f;	
